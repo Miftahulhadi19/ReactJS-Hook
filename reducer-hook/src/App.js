e3c16b5730a305d802a452021c8ffe266bfde3fc
Miftahulhadi19 <miftahul.hadi19clear$ clearcleargit config --global user.email miftahul.hadi19>
2023-03-14 21:43:55 +0700

    menambah file belajar useReducer

--- reducer-hook/src/App.js
+++ reducer-hook/src/App.js
@@ -1,49 +1,49 @@
-import React, { useReducer } from 'react'
-import './App.css'
-import CounterOne from './components/CounterOne'
-import CounterTwo from './components/CounterTwo'
-import CounterThree from './components/CounterThree'
-import ComponentA from './components/ComponentA'
-import ComponentB from './components/ComponentB'
-import ComponentC from './components/ComponentC'
-import DataFetchingOne from './components/DataFetchingOne'
-import DataFetchingTwo from './components/DataFetchingTwo'
+import React, { useReducer } from 'react';
+import './App.css';
+import CounterOne from './component/CounterOne';
+import CounterTwo from './component/CounterTwo';
+import CounterThree from './component/CounterThree';
+import ComponentA from './component/ComponentA';
+import ComponentB from './component/ComponentB';
+import ComponentC from './component/ComponentC';
+import DataFetchingOne from './component/DataFetchingOne';
+import DataFetchingTwo from './component/DataFetchingTwo';
 
-const initialState = 0
+const initialState = 0;
 const reducer = (state, action) => {
-	switch (action) {
-		case 'increment':
-			return state + 1
-		case 'decrement':
-			return state - 1
-		case 'reset':
-			return initialState
-		default:
-			return state
-	}
-}
+  switch (action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'reset':
+      return initialState;
+    default:
+      return state;
+  }
+};
 
-export const CountContext = React.createContext()
+export const CountContext = React.createContext();
 
 function App() {
-	const [count, dispatch] = useReducer(reducer, initialState)
-	return (
-		<CountContext.Provider
-			value={{ countState: count, countDispatch: dispatch }}
-		>
-			<div className="App">
-				{/* <CounterOne /> */}
-				{/* <CounterTwo /> */}
-				{/* <CounterThree /> */}
-				{/* {count} */}
-				{/* <ComponentA /> */}
-				{/* <ComponentB /> */}
-				{/* <ComponentC /> */}
-				{/* <DataFetchingOne /> */}
-				<DataFetchingTwo />
-			</div>
-		</CountContext.Provider>
-	)
+  const [count, dispatch] = useReducer(reducer, initialState);
+  return (
+    <CountContext.Provider
+      value={{ countState: count, countDispatch: dispatch }}
+    >
+      <div className='App'>
+        {/* <CounterOne /> */}
+        {/* <CounterTwo /> */}
+        {/* <CounterThree /> */}
+        {/* {count} */}
+        {/* <ComponentA /> */}
+        {/* <ComponentB /> */}
+        {/* <ComponentC /> */}
+        {/* <DataFetchingOne /> */}
+        <DataFetchingTwo />
+      </div>
+    </CountContext.Provider>
+  );
 }
 
-export default App
+export default App;
